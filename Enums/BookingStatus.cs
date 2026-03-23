a1dbebad3dbf5547d0ba73cd2a717be9326cfb9d
Bozhidar Enev <enevbozhidar@gmail.com>
2026-03-23 20:05:13 +0200

    added classes in Enums and Models

---
+++ Enums/BookingStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travel_Agency_System.Enums
+{
+    internal class BookingStatus
+    {
+    }
+}
---
+++ Models/Trip.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travel_Agency_System.Models
+{
+    internal class Trip
+    {
+    }
+}
