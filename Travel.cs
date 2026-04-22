58aa63b32418b79f7dd6b10f8c36ba48ee63e427
dqdokukuruz <Dqdokukuruz@gmail.com>
2026-04-22 09:51:37 +0300

    kod za json faila

---
+++ Travel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Class1
+{
+    public class Travel
+    {
+        public int Id { get; set; }
+        public string Destination { get; set; }
+        public decimal Price { get; set; }
+    }
+}
